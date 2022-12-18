332bdfb29792f3c7f336fb51fb546356ea1ba3c4
Serpions79 <randommolten007@gmail.com>
2022-12-18 12:13:42 +0100

    first commit

---
+++ juegardo.py
@@ -0,0 +1,160 @@
+import random
+
+class Objeto:
+    ataque=0
+    defensa=0
+    resistencia=0
+    def __init__(self,nombre,ataque,defensa,resistencia,valor):
+        self.nombre = nombre
+        self.ataque = ataque
+        self.defensa = defensa
+        self.resistencia = resistencia
+        self.valor = valor
+Monedero=Objeto("Monedero",0,0,0,11)
+Objetos=[Monedero]
+def buscarObjeto(nombre):
+    encontrado=0
+    i=0
+    while i<len(Objetos) and encontrado==0:
+        if Objetos[i].nombre ==nombre:
+            encontrado=1
+        else:
+            i=i+1
+    if encontrado==1:
+        return 1
+    else:
+        return 0
+print("Bienvenido a caminos distintos donde tendras que escoger tu propio camino segun que opciones escogas")
+print("Empezaremos con algo sencillo ")
+print("Elige 1 o 2 ")
+Algo=int(input())
+if Algo==1:
+    print("Has escogido el numero 1 apareceras en una selva" )
+elif Algo==2:
+     print("Has escogido el numero 2 apareceras en un desierto")
+print("Vaya has aparecido en este bioma peculiar ten te puedo dar 3 objetos pero solo puedes escoger 1 para sobrevivir ")
+print("Si quieres una cuerda escoge 1 si quieres una bengala escoge 2 si quieres un machete escoge 3")
+Object=int(input())
+if  Object==1:
+    print("Has escogido la cuerda buena eleccion ")
+    Objetos.append(Objeto("Cuerda",5,6,6,6))
+elif Object==2:
+     print("Has escogido la bengala magnifica idea ") 
+     Objetos.append(Objeto("Bengala",2,7,8,7))
+elif Object==3:
+     print("Has escogido el machete, si te soy sincero yo tambien lo habria escogido ")
+     Objetos.append(Objeto("Machete",8,6,4,5))
+print("Ahora que ya estamos listos vamonos")
+print("4 HORAS DESPUES")
+print("Ya se esta haciendo de noche. Vaya mira lo que veo es una cabanya de madera a tu derecha pero veo una cueva tambien a tu izquierda ")
+print("Escoge 1 para pasar la noche en la cabanya o escoge 2 para pasar la noche en la cueva")
+Refugio=int(input())
+if Refugio==1:
+   print("Has escogido la cabanya me parece correcto ")
+elif Refugio==2:
+     print("Has escogido la cueva buena eleccion ")
+print("HAS PASADO LA 1r NOCHE FIN DE LA PARTIDA")
+print("SI TE QUEDASTE CON GANAS DE MAS PODRAS JUGAR LA SEGUNDA PARTE DENTRO DE 1 ANYO")
+print("INSERTAR MUSICA DE VIDEOJUEGOS")
+print("Oh vaya estas aqui aun")
+print("Bueno te dejo jugar la segunda parte")
+print("MUSICA DE ACCION")
+numerorandom=random.uniform(0,100)
+if 30>=numerorandom:
+    print("Oh no te estan persiguiendo unos lobos salvajes y es de noche")
+    if buscarObjeto("Bengala")==1:
+        print("Teniendo la bengala puedes utilizarla, Quieres utilizarla ahora? 1 si 2 no") 
+        Hola=int(input())
+        if Hola==1:
+           print("Has utilizado la bengala y los lobos se han ido")
+        elif Hola==2:
+            print("Los lobos te han dejado a media vida pero se han ido")
+    else:
+        print("Como no puedes hacer nada los lobos te han dejado a media vida")
+print("Ya llevas 3 horas y aun no has comido pero mira lo que veo es un ciervo")
+if buscarObjeto("Machete")==1:
+    print("Puedes utilizar el machete para comer el ciervo (1) o puedes seguir caminando y buscar otra comida (2)")
+    A=int(input())
+    if A==1:
+        print("Has utilizado el Machete... pero el ciervo a escapado")
+    elif A==2:
+        print("Bien hecho, como podrias matar a un ciervo tu solo con un machete?")
+print("Mira has encontrado otra casa abandonada")
+print("Quieres ir a investigar esa casa?")
+print("1 SI 2 NO")
+SIUUU=int(input())
+if SIUUU==1:
+    print("Has entrado a la casa y mira habia un monton de comida toda esta comida nos bastara para 1 semana")
+elif SIUUU==2:
+    print("Mejor no entraremos a esa casa pero parece oler bastante bien seguro que no quieres entrar a la casa? Pon 1 si al final quieres ir o pon 2 si no quieres ir")
+    B=int(input())
+    if B==1:
+        print("Has entrado a la casa y mira habia un monton de comida toda esta comida nos bastara para 1 semana")
+    elif B==2:
+        print("Ok no iremos a la casa abandonada")
+        
+print("Hola, David malo")
+
+print("Vaya mira un pozo pero que hara un pozo por estas tierras?")
+print("Para ir a mirar el pozo pulsa 1 para pasar del pozo pulsa 2")
+Q=int(input())
+if Q==1:
+    print("Mira que precioso es el pozo oye te acuerdas cuando la gente tiraba monedas a un pozo para cumplir un deseo")
+    print("Pulsa 1 para tirar un deseo o pulsa 2 para no hacer nada")
+    quack=int(input())
+    if quack==1:
+        print("Tiraste la moneda pero que esta pasando?")
+        print("Parece como si hubiera un tornado rapido ve a esa cueva para ponernos a salvo pero pareze que el pozo comienza a hacer cosas raras")
+        print("escoge 1 para ponerte a salvo escoge 2 para mirar lo que pasa en ese pozo")
+        E=int(input())
+        if E==1:
+            print("Aqui estamos sanos y salvos")
+            print("Escuche un ruido extranyo, ¿quieres ir a investigar que es eso?")
+            print("1 para investigar 2 para permanezer quieto")
+            sus=int(input())
+            if sus==1:
+                print("No me lo puedo creer es un Pikachu pero esto no tiene sentido esas cosas no son reales")
+                print("FIN DE LA PARTE 2")
+            elif sus==2:
+                print("Pareze que ya se fue el tornado")
+                print("Que raro el pozo a desaparecido me pregunto que habra pasado")
+                print("FIN DE LA PARTE 2")
+        elif E==2:
+            print("Voz extraña: Ayudanos necesitamos tu ayuda")
+            print("Voz extraña: Necesito que te tires al pozo")
+            print("Escoge 1 para tirarte al pozo o escoge 2 para mejor ir a la cueva")
+            agua=int(input())
+            if agua==1:
+                print("TE HAS TIRADO AL POZO")
+                print("Puedes observar como la oscuridad de la parte profunda del pozo poco a poco se esta haciendo mas clara")
+                print("FIN DE LA PARTE 2")
+            elif agua==2:
+                print("Corres como puedes hacia la cueva pero el tornado te pillo")
+                print("HAS MUERTO")
+                print("Puedes volver a intentarlo y no morir en el intento")
+    elif quack==2:
+        print("Sigues tu camino como si nada")
+        print("Y escuchas un ruido extraño")
+        print("*Se la la vuelta y observa que no esta el pozo mas*")
+        print("Que raro")
+        print("Oyes eso es un ruido en esos arbustos")
+        print("Parecen peligrosos pero ¿valdra la pena mirar que es?")
+        print("Pulsa 1 para mirar que es o pulsa 2 para no arriesgarte")
+        T=int(input())
+        if T==1:
+            print("Es un Charmander pero eso no era un Pokemon")
+            print("¿Que hace esta cosa aqui acaso no era un videojuego?")
+            print("FIN DE LA PARTE 2")
+        elif T==2:
+            print("Que acaba de pasar HAY UN MURO DE FUEGO DELANTE")
+            print("FIN DE LA PARTE 2")
+elif Q==2:
+    print("Te das cuenta que el pozo desaparecio magicamente")
+    print("FIN DE LA PARTE 2")
+"""
+RULETA DE LA SUERTE :)
+Buscar una funcion que nos de un numero aleatorio del 1 al 100 (solo en el caso de utilizar porcentajes) 
+Ejemplo: Que pase un 30% de probabilidad de que ocurra cierta acción
+Cada vez que sea de noche alla un 30% de que alla lobos 
+Si el numero aleatorio es menor o igual a 30 pues te aparece lo de los lobos o si no continua la historia 
+"""
