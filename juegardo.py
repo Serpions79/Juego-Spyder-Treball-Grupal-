f60b7a4d9eead2986155f5543e83699fab343222
Serpions79 <randommolten007@gmail.com>
2023-02-01 19:19:42 +0100

    Creacion clase personaje

--- juegardo.py
+++ juegardo.py
@@ -1,5 +1,30 @@
 import random
 
+class Personaje:
+    nombre=""
+    vida=0
+    ataque_actual=0
+    defensa_actual=0
+    objetos=[]
+    def __init__(self,nombre,vida,ataque_actual,defensa_actual,objetos):
+        self.nombre = nombre
+        self.vida = vida
+        self.ataque_actual = ataque_actual
+        self.defensa_actual = defensa_actual
+        self.objetos = objetos
+    def buscarObjeto(nombre,objetos):
+        encontrado=0
+        i=0
+        while i<len(objetos) and encontrado==0:
+            if objetos[i].nombre ==nombre:
+                encontrado=1
+            else:
+                i=i+1
+        if encontrado==1:
+            return 1
+        else:
+            return 0
+
 class Objeto:
     ataque=0
     defensa=0
@@ -10,20 +35,25 @@ class Objeto:
         self.defensa = defensa
         self.resistencia = resistencia
         self.valor = valor
-Monedero=Objeto("Monedero",0,0,0,11)
-Objetos=[Monedero]
-def buscarObjeto(nombre):
-    encontrado=0
-    i=0
-    while i<len(Objetos) and encontrado==0:
-        if Objetos[i].nombre ==nombre:
-            encontrado=1
-        else:
-            i=i+1
-    if encontrado==1:
-        return 1
-    else:
-        return 0
+class Enemigo:
+    nombre=""
+    vida=0
+    ataque=0
+    defensa=0
+    def __init__(self,nombre,vida,ataque,defensa):
+        self.nombre = nombre
+        self.vida = vida
+        self.ataque = ataque
+        self.defensa = defensa
+        
+
+
+monedero=Objeto("monedero",0,0,0,11)
+
+enemigos=[]
+
+player=Personaje("",100,0,0,[])
+
 print("Bienvenido a caminos distintos donde tendras que escoger tu propio camino segun que opciones escogas")
 print("Empezaremos con algo sencillo ")
 print("Elige 1 o 2 ")
@@ -37,13 +67,13 @@ print("Si quieres una cuerda escoge 1 si quieres una bengala escoge 2 si quieres
 decision=int(input())
 if  decision==1:
     print("Has escogido la cuerda buena eleccion ")
-    Objetos.append(Objeto("Cuerda",5,6,6,6))
+    player.objetos.append(Objeto("Cuerda",1,0,6,5))
 elif decision==2:
      print("Has escogido la bengala magnifica idea ") 
-     Objetos.append(Objeto("Bengala",2,7,8,7))
+     player.objetos.append(Objeto("Bengala",2,0,5,7))
 elif decision==3:
      print("Has escogido el machete, si te soy sincero yo tambien lo habria escogido ")
-     Objetos.append(Objeto("Machete",8,6,4,5))
+     player.objetos.append(Objeto("Machete",6,0,5,4))
 print("Ahora que ya estamos listos vamonos")
 print("4 HORAS DESPUES")
 print("Ya se esta haciendo de noche. Vaya mira lo que veo es una cabanya de madera a tu derecha pero veo una cueva tambien a tu izquierda ")
@@ -60,19 +90,23 @@ print("Oh vaya estas aqui aun")
 print("Bueno te dejo jugar la segunda parte")
 print("MUSICA DE ACCION")
 numerorandom=random.uniform(0,100)
+
 if 30>=numerorandom:
+    enemigos.append(Enemigo("Lobo",5,6,4))
     print("Oh no te estan persiguiendo unos lobos salvajes y es de noche")
-    if buscarObjeto("Bengala")==1:
+    if player.buscarObjeto("Bengala",player.objetos)==1:
         print("Teniendo la bengala puedes utilizarla, Quieres utilizarla ahora? 1 si 2 no") 
         decision=int(input())
         if decision==1:
            print("Has utilizado la bengala y los lobos se han ido")
+           
         elif decision==2:
-            print("Los lobos te han dejado a media vida pero se han ido")
+            print("Como no has utilizado la Bengala PREPArATE PARA COMBATIR")
     else:
-        print("Como no puedes hacer nada los lobos te han dejado a media vida")
+        print("Como no puedes hacer nada los lobos te han dejado a media vida PREPARATE POR QUE LA VAS A PASAR MAL")
+        print("EMPIEZA EL COMBATE")
 print("Ya llevas 3 horas y aun no has comido pero mira lo que veo es un ciervo")
-if buscarObjeto("Machete")==1:
+if player.buscarObjeto("Machete",player.objetos)==1:
     print("Puedes utilizar el machete para comer el ciervo (1) o puedes seguir caminando y buscar otra comida (2)")
     decision=int(input())
     if decision==1:
@@ -145,13 +179,15 @@ if decision==1:
                                 print("1 izquierda 2 derecha")
                                 decision=int(input())
                                 if decision==1:
-                                   print("Ves una especie de figara extraña y va hacia ti")
+                                   print("Ves una especie de figura extraña y va hacia ti")
                                    print("1 quedarte quieto 2 huir")
                                    decision=int(input())
                                    if decision==1:
                                        print("Es pikachu y te ha traido una baya")
                                        print("Y el señor de antes estaba detras tuyo y te deice que el es OAK EL PROFESOR POKEMON")
                                        print("FIN DE LA 3r PARTE")
+                                   elif decision==2:
+                                       print("Huiste por un camino un tanto extraño")
                     elif decision==2:
                         print("Te quedas en la cueva y escuchas un rugido")
                         print("Es un CHARIZARD")
@@ -182,7 +218,7 @@ if decision==1:
                 print("Que raro el pozo a desaparecido me pregunto que habra pasado")
                 print("FIN DE LA PARTE 2")
                 print("Hay una luz al final de este camino que brilla demasiado")
-                print("David continua")
+                print("DAVID CONTINUA")
         elif decision==2:
             print("Voz extraña: Ayudanos necesitamos tu ayuda")
             print("Voz extraña: Necesito que te tires al pozo")
@@ -200,7 +236,7 @@ if decision==1:
     elif decision==2:
         print("Sigues tu camino como si nada")
         print("Y escuchas un ruido extraño")
-        print("*Se la la vuelta y observa que no esta el pozo mas*")
+        print("*Se da la vuelta y observa que no esta el pozo mas*")
         print("Que raro")
         print("Oyes eso es un ruido en esos arbustos")
         print("Parecen peligrosos pero ¿valdra la pena mirar que es?")
@@ -210,7 +246,7 @@ if decision==1:
             print("Es un Charmander pero eso no era un Pokemon")
             print("¿Que hace esta cosa aqui acaso no era un videojuego?")
             print("FIN DE LA PARTE 2")
-            print("David continua")
+            print("DAVID CONTINUA")
         elif decision==2:
             print("Que acaba de pasar HAY UN MURO DE FUEGO DELANTE")
             print("FIN DE LA PARTE 2")
@@ -219,10 +255,21 @@ elif decision==2:
     print("Te das cuenta que el pozo desaparecio magicamente")
     print("FIN DE LA PARTE 2")
     print("David continua")
+
+
+
+
 """
 RULETA DE LA SUERTE :)
 Buscar una funcion que nos de un numero aleatorio del 1 al 100 (solo en el caso de utilizar porcentajes) 
 Ejemplo: Que pase un 30% de probabilidad de que ocurra cierta acción
 Cada vez que sea de noche alla un 30% de que alla lobos 
 Si el numero aleatorio es menor o igual a 30 pues te aparece lo de los lobos o si no continua la historia 
+
+
+MEJORAR LA CLASE OBJETO (Hacer que el objeto sea equipable o no) Separar el objeto que tienes equipado con los de la bolsa de objetos
+Y añadir tipos en los objetos 
+añadir la clase enemigo (Nombre, vida, ataque, defensa,)
+Añadir una clase jugador para añadir a nosotros (Ayuda)
+
 """
